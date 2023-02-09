8df21fc5d30eceef64f22d9078246d9ea922db15
Timothy Pfafman <tim@pfafman.com>
2023-02-09 07:22:15 -0700

    add mobile app files

---
+++ mobile-config.js
@@ -0,0 +1,14 @@
+
+App.info({
+  id: 'com.wisomm.test',
+  name: 'Wisomm Test App',
+  version: "1.9.1",
+  buildNumber: 190
+});
+
+
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');
+
+
