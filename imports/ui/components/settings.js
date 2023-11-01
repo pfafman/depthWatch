39d0d4954ef39c17049351265b5d766605289cf0
Timothy Pfafman <tim@pfafman.com>
2023-11-01 14:49:39 -0600

    tanks are almost level withing 1/4 inch

--- imports/ui/components/settings.js
+++ imports/ui/components/settings.js
@@ -5,30 +5,29 @@ tankCapacity = 3236;
 
 gallonsPerInch = 3 * tankCapacity/tankDepth;
 
-gallonsPerInchAbove19 = 57.6;
+gallonsPerInchBelow9 = 56.9;
 
-maxDepth    = 57;   // Where Sensor was
+oldMaxDepth    = 57;   // Where Sensor was
 newMaxDepth = 67;   // Where Sensor is
 
 minValidReading = 67 - 59;  // 8 inches
 
-capacity = tankCapacity * 3 + 11*1.36*2;
+capacity = tankCapacity * 3; + gallonsPerInchAbove9; //11*1.36*2;
 
 gallonsInTanks = (sensorReading, time) => {
-    let gallons = tankCapacity * 3 + 11*1.36*2;
+    let gallons = capacity;
     let theMaxDepth = newMaxDepth;
 
     if (moment(time).isBefore(moment("2023-05-10"))) {
-        theMaxDepth = maxDepth;
+        theMaxDepth = oldMaxDepth;
     }
 
     if (sensorReading < 8) {
-        gallons = tankCapacity * 3 + 11*1.36*2;
-    } else if (sensorReading < 19) {
-        gallons = tankCapacity * 3 + 11*1.36*2 - (sensorReading - 8) * gallonsPerInchAbove19;
+        gallons = capacity;
+    // } else if (sensorReading < 9) {
+    //     gallons = capacity - (sensorReading - 8) * gallonsPerInchBelow9;
     } else {
-        gallons = tankCapacity * 3 + 11*1.36*2 - (19 - 8) * gallonsPerInchAbove19;
-        gallons -= (sensorReading-19) * gallonsPerInch;
+        gallons = (theMaxDepth-sensorReading) * gallonsPerInch;
     }
 
     return Math.round(gallons);
